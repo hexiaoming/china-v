cdd1851e0736a6cd20357a8f51af9893091c4103
wangbailin <wangbailin@limijiaoyin.com>
2014-07-14 21:00:10 +0800

    merge

--- base/urls.py
+++ base/urls.py
@@ -2,19 +2,12 @@ from django.conf.urls import patterns, include, url
 from django.conf import settings
 
 urlpatterns = patterns('',
-<<<<<<< HEAD
     url(r'^ajax-upload/', include('ajax_upload.urls')),
     url(r'', include('backend.urls')),
     url(r'^backend/students/', include('students.urls')),
     url(r'^students/', include('students.front_urls'))
-=======
-    # Examples:
-    # url(r'^$', 'chinav.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-    # url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('backend.urls')),
     url(r'^', include('promotion.urls'))
->>>>>>> 5db5252e9e3ef21cfef65789b52cb81845c78c99
 )
 
 
