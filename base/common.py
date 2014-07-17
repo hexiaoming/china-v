5492c0d44c60bd4dd7ea6f4955d43188597138ee
jarvys <yuhan534@126.com>
2014-07-17 12:08:58 +0800

    refine static files management

--- base/common.py
+++ base/common.py
@@ -76,10 +76,10 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "_static")
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "_media")
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "assets"),
