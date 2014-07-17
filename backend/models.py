8c5df75363d5f3b965efe1aed3d83e893852fd8c
wangbailin <wangbailin@limijiaoyin.com>
2014-07-18 03:42:35 +0800

    csrf

--- backend/models.py
+++ backend/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 
-class costum_service(models.Model):
-	problem_type = models.TextField()
+class Costum_service(models.Model):
+	problem_types = models.TextField()
 	name = models.CharField(max_length=100)
 	phone = models.CharField(max_length=15)
 	email = models.CharField(max_length=30)
