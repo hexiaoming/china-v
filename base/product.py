637381e7a83047ef5da91dbf859f5ffa488b6533
jarvys <yuhan534@126.com>
2014-07-17 12:05:47 +0800

    polish

--- base/product.py
+++ base/product.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'zuvp4036q9zpc!)0sac=qhk!%udh^5dcavrr@lc6c)m41f3nz%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 TEMPLATE_DEBUG = False
-ALLOWED_HOSTS = [] 
+ALLOWED_HOSTS = ['wx.jdb.cn'] 
 
 from base.common import *
 from base.loggers import LOGGING
